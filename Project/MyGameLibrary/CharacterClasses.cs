8ec803faba18226713d351b3faed5c971b236077
dwunst <49620071+dwunst@users.noreply.github.com>
2021-10-29 16:13:45 -0500

    Added file outline for Multiple Character Classes.

---
+++ Project/MyGameLibrary/CharacterClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+	public class CharacterClasses : Character
+	{
+		public int Health { get; private set; }
+		public int MaxHealth { get; private set; }
+		private float Strength;
+
+		public Default(Vector2 initPos, Collider collider) : base(initPos, collider)
+		{
+			MaxHealth = 20;
+			Strength = 2;
+			Health += MaxHealth;
+        }
+
+		public Paladin(Vector2 initPos, Collider collider) : base(initPos, collider)
+		{
+			MaxHealth = 30;
+			Strength = 4;
+			Health += MaxHealth;
+		}
+
+
+		public Monk(Vector2 initPos, Collider collider) : base(initPos, collider)
+		{
+			MaxHealth = 15;
+			Strength = 1.5;
+			Health += MaxHealth;
+		}
+
+		public Assassin(Vector2 initPos, Collider collider) : base(initPos, collider)
+		{
+			MaxHealth = 10;
+			Strength = 1;
+			Health += MaxHealth;
+		}
+	}
+}

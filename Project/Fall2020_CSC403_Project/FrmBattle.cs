75a95b541d3d89a7b0b547bbc03f3ae056629def
andrew redfield <reapermail1998@gmail.com>
2021-11-10 19:48:12 -0600

    Changed Flee and Defense options so now they work.

--- Project/Fall2020_CSC403_Project/FrmBattle.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.cs
@@ -127,10 +127,132 @@ namespace Fall2020_CSC403_Project {
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //this.Hide();
-            player.AlterHealth(-1);
-            instance = null;
-            Close();
+            /* //this.Hide();
+             player.AlterHealth(-1);
+             instance = null;
+             Close();
+            */
+            OnEscape();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OnDefend(-4);
+        }
+
+        public void OnDefend(int damage)
+        {
+            int ClassID = 1;
+            double defense = 0.0;
+            if (ClassID == 1)
+            {
+                defense = 0.75;
+            }
+
+            else
+            {
+                defense = 0.5;
+            }
+            DefenseCalc(damage, defense);
+        }
+
+        public void DefenseCalc(int damage, double defense)
+        {
+            double DamageReduction = (damage * defense);
+            double TotalDamage = (damage - DamageReduction);
+            //convert double to int
+            int fuckshit = (int)(Math.Floor(TotalDamage));
+            player.AlterHealth(fuckshit);
+            enemy.AlterHealth(fuckshit*2);
+            UpdateHealthBars();
+
+            if (enemy.Health <= 0)
+            {
+                enemy.IsAlive = false;
+                instance = null;
+                Close();
+            }
+            if (player.Health <= 0)
+            {
+                //show game over screen
+                this.Hide();
+                instance = null;
+                GameOver GO = new GameOver();
+                GO.Show();
+            }
+
+            //find thing that update visual then do
+
+            //use this for Default class because Gun.
+            //           enemy.AlterHealth(-20);
+
+        }
+
+        public void OnEscape()
+        {
+            Random PlayerR = new Random();
+            Random EnemyR = new Random();
+
+            int PlayerEscapeChance = PlayerR.Next(1, 5);
+            int EnemyPursuitChance = PlayerR.Next(1, 5);
+
+            int ClassID = 1;
+
+            if (ClassID == 3)
+            {
+                PlayerEscapeChance += 1;
+                EscapeRoute(PlayerEscapeChance, EnemyPursuitChance);
+            }
+
+            else
+            {
+                EscapeRoute(PlayerEscapeChance, EnemyPursuitChance);
+            }
+        }
+
+        //Optimize usage of the method for closing the window
+        public void EscapeRoute(int PlayerEscapeChance, int EnemyPursuitChance)
+        {
+            if (PlayerEscapeChance > EnemyPursuitChance)
+            {
+                instance = null;
+                this.Hide();
+            }
+
+            else if (EnemyPursuitChance > PlayerEscapeChance)
+            {
+                //remove the multiplier
+                int PursuitDamage = (0 - ((EnemyPursuitChance - PlayerEscapeChance) * 2));
+                //send to the alter player health thing
+                player.AlterHealth(PursuitDamage);
+                UpdateHealthBars();
+                instance = null;
+                this.Hide();
+            }
+
+            else
+            {
+                double fuckshit2 = (0 - (PlayerEscapeChance / 2));
+                int EscapeDamage = (int)Math.Floor(fuckshit2);
+                //send to the alter player health thing
+                player.AlterHealth(EscapeDamage);
+                UpdateHealthBars();
+                instance = null;
+                this.Hide();
+            }
+
+            if (player.Health <= 0)
+            {
+                //show game over screen
+                this.Hide();
+                instance = null;
+                GameOver GO = new GameOver();
+                GO.Show();
+
+
+            }
+        }
+
+        
     }
 }

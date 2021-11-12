83bc83f5d5a6e375a787cb52dd9c6312276f541e
Luke Jobe <Lionpaw75@gmail.com>
2021-11-12 17:01:19 -0600

    Changed Class select screen

--- Project/Fall2020_CSC403_Project/FmClasses.Designer.cs
+++ Project/Fall2020_CSC403_Project/FmClasses.Designer.cs
@@ -47,9 +47,10 @@ namespace Fall2020_CSC403_Project
             // 
             this.button1.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Button_6;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(12, 365);
+            this.button1.Location = new System.Drawing.Point(74, 580);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 52);
+            this.button1.Size = new System.Drawing.Size(189, 64);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -58,9 +59,10 @@ namespace Fall2020_CSC403_Project
             // 
             this.button2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Button_3;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(209, 365);
+            this.button2.Location = new System.Drawing.Point(486, 580);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 52);
+            this.button2.Size = new System.Drawing.Size(189, 64);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -69,9 +71,10 @@ namespace Fall2020_CSC403_Project
             // 
             this.button3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Button_4;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(414, 365);
+            this.button3.Location = new System.Drawing.Point(827, 580);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(142, 52);
+            this.button3.Size = new System.Drawing.Size(189, 64);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -80,66 +83,78 @@ namespace Fall2020_CSC403_Project
             // 
             this.button4.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Button_5;
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.Location = new System.Drawing.Point(619, 365);
+            this.button4.Location = new System.Drawing.Point(1184, 580);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(142, 52);
+            this.button4.Size = new System.Drawing.Size(189, 64);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.OG_DI;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Orange_Girl_Shimmer;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Location = new System.Drawing.Point(13, -49);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(160, 289);
+            this.pictureBox1.Size = new System.Drawing.Size(389, 672);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.AM_DI;
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Apple_Man_Shimmer;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(209, 51);
+            this.pictureBox2.Location = new System.Drawing.Point(389, -14);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(160, 289);
+            this.pictureBox2.Size = new System.Drawing.Size(387, 658);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.MM_DI;
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Monk_Man_Shimmer;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(414, 51);
+            this.pictureBox3.Location = new System.Drawing.Point(765, 3);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(160, 289);
+            this.pictureBox3.Size = new System.Drawing.Size(329, 595);
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.TG_DI;
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.Tomato_Girl_Shimmer;
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(619, 51);
+            this.pictureBox4.Location = new System.Drawing.Point(1102, 3);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(160, 289);
+            this.pictureBox4.Size = new System.Drawing.Size(329, 595);
             this.pictureBox4.TabIndex = 8;
             this.pictureBox4.TabStop = false;
             // 
             // FmClasses
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.World_1;
+            this.ClientSize = new System.Drawing.Size(1454, 689);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FmClasses";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();

3c6d8ef2d0acfd84f7dff29244a0fcb57920c718
Cameron Mitchell <clm8043@gmail.com>
2021-11-11 18:47:56 -0600

    Add speed to Enemy constructor

--- Project/MyGameLibrary/Enemy.cs
+++ Project/MyGameLibrary/Enemy.cs
@@ -60,7 +60,7 @@ namespace Fall2020_CSC403_Project.code {
         /// </summary>
         /// <param name="initPos">this is the initial position of the enemy</param>
         /// <param name="collider">this is the collider for the enemy</param>
-        public Enemy(Vector2 initPos, Collider collider, Point boundary1 = default(Point), Point boundary2 = default(Point)) : base(initPos, collider) {
+        public Enemy(Vector2 initPos, Collider collider, Point boundary1 = default(Point), Point boundary2 = default(Point), int speed = 0) : base(initPos, collider) {
             EnemyPosition = initPos;
             EnemyCollider = collider;
             IsAlive = true;
@@ -75,13 +75,13 @@ namespace Fall2020_CSC403_Project.code {
                 {
                     m_moveDirection = MoveDirection.Vertical;
                     m_enemyBoundaries = new int[2] { boundary1.Y, boundary2.Y };
-                    EnemyMoveSpeed = new Vector2(0, 2);
+                    EnemyMoveSpeed = new Vector2(0, speed);
                 }
                 else
                 {
                     m_moveDirection = MoveDirection.Horizontal;
                     m_enemyBoundaries = new int[2] { boundary1.X, boundary2.X };
-                    EnemyMoveSpeed = new Vector2(2, 0);
+                    EnemyMoveSpeed = new Vector2(speed, 0);
                 }
             }
         }
